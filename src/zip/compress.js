88ae076dcf41b160e1c3bfc0d5d01659d21de7b3
ArtemPotapchick <artem.potapchick@gmail.com>
2022-12-05 22:38:03 +0300

    feat:implemented Zlib (src/zip) subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import zlib from 'zlib'
+import { fileURLToPath } from "url";
+import path from "path";
+import * as fs from 'fs'
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const gzip = zlib.createGzip();
+  const sourceFile = fs.createReadStream(path.join(__dirname,'files','fileToCompress.txt'))
+  const destFile = fs.createWriteStream(path.join(__dirname,'files','archive.gz'))
+  sourceFile.pipe(gzip).pipe(destFile);
+  // Write your code here
 };
 
 await compress();
