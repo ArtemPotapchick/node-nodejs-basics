65726112e55161c7dc6acfd4e18a91ee78ea6323
ArtemPotapchick <artem.potapchick@gmail.com>
2022-12-05 22:33:59 +0300

    feat:implemented File system (src/fs) subtask

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import * as path from 'path';
+import fs from "fs";
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, 'files'))){
+      throw 'FS operation failed';
+    }
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+      files.forEach(file => {
+        console.log(file);
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  // Write your code here
 };
 
 await list();
