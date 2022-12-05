ea2eac9f120b8d815a2216ebc025845685a8f13b
ArtemPotapchick <artem.potapchick@gmail.com>
2022-12-05 22:38:35 +0300

    feat:implemented Worker Threads (src/wt) subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import * as os from 'os';
+import { Worker } from 'worker_threads';
+import path from "path";
+import { fileURLToPath } from "url";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  let num = 10;
+  const cpuCount = os.cpus().length;
+  Promise.all(Array.from(Array(cpuCount), (x, i) => i).map(i =>
+    new Promise((resolve, reject) => {
+      const worker = new Worker(path.join(__dirname, 'worker.js'), {
+        workerData: num + i
+      });
+      worker.on('message', value => {
+        resolve({ status: 'resolved', data: value });
+      });
+      worker.on('error', err => {
+        resolve({ status: 'error', data: null });
+      });
+    }))).then(results=>console.log(results));
 };
 
 await performCalculations();
