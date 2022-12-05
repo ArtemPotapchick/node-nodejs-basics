f35607a1ef76dc689a8dc601fc3b6a108fef259f
ArtemPotapchick <artem.potapchick@gmail.com>
2022-12-05 22:35:18 +0300

    feat:implemented Command line interface subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  let argvArr = [];
+  process.argv.map((item, index) => {
+    if (item.startsWith('--')) {
+      argvArr = [...argvArr, `${item} is ${process.argv[index + 1]}`];
+    }
+  });
+  console.log(argvArr.join(', '));
 };
 
 parseArgs();
